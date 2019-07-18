1d11f2b2e1abf54f4f69aa4c7453724704f10627
Sara Hentzel <sara_hentzel@sil.org>
2019-07-18 12:12:30 -0500

    Process new s3 file - update filesize and duration in mediafiles record

---
+++ src/index.js
@@ -0,0 +1,114 @@
+const https = require("https");
+const mp3 = require("mp3-duration");
+const path = require("path");
+
+const host = process.env.SIL_TR_HOST;
+const stagepath = process.env.SIL_TR_URLPATH;
+
+exports.handler = async event => {
+  console.log("hello!");
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, " ")
+  );
+  const filename = path.basename(key);
+  const region = event.Records[0].awsRegion;
+  const filesize = event.Records[0].s3.object.size;
+  console.log(region);
+  console.log(bucket);
+  console.log(key);
+  console.log(host);
+  console.log(stagepath);
+
+  function getMedia() {
+    return new Promise((resolve, reject) => {
+      // options for API request
+      var options = {
+        host: host,
+        path: stagepath + "/api/mediafiles/fromfile/" + filename,
+        method: "GET"
+      };
+
+      const req = https.request(options, res => {
+        console.log("media statusCode:", res.statusCode);
+        if (res.statusCode === 404) {
+          //not found
+          reject(res.statusCode);
+        }
+        res.on("data", d => {
+          resolve(JSON.parse(d));
+        });
+      });
+
+      req.on("error", e => {
+        console.log("media error:", e);
+        reject(e);
+      });
+
+      // req.write(postData);
+
+      req.end();
+    });
+  }
+  async function patchMedia(id, filesize, duration) {
+    return new Promise((resolve, reject) => {
+      // options for API request
+      var options = {
+        host: host,
+        path:
+          stagepath + "/api/mediafiles/" +
+          id +
+          "/fileinfo/" +
+          filesize +
+          "/" +
+          duration,
+        method: "PATCH"
+      };
+
+      const req = https.request(options, res => {
+        console.log(options);
+        console.log("media statusCode:", res.statusCode);
+        if (res.statusCode === 404) {
+          //not found
+          reject(res.statusCode);
+        }
+        res.on("data", d => {
+          resolve(JSON.parse(d));
+        });
+      });
+
+      req.on("error", e => {
+        console.log("media error:", e);
+        reject(e);
+      });
+
+      // req.write(postData);
+
+      req.end();
+    });
+  }
+  async function getFile() {
+    const aws = require("aws-sdk");
+    const s3 = new aws.S3(); // Pass in opts to S3 if necessary
+    var params = {
+      Bucket: bucket, // your bucket name,
+      Key: key // path to the object you're looking for
+    };
+    const data = await s3.getObject(params).promise();
+    return data.Body;
+  }
+
+  try {
+    var x = await getMedia();
+    console.log("ID" + x.data.id);
+    var duration = await mp3(await getFile());
+    console.log("Your file is " + duration + " seconds long");
+    //patch it
+    var x = await patchMedia(x.data.id, filesize, duration);
+    return x;
+  } catch (e) {
+    console.log("getMedia catch");
+    console.log(e);
+    return e;
+  }
+};
