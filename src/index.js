590ffff481a3c22a883dd921058988083388c8cf
Sara Hentzel <sara_hentzel@sil.org>
2023-02-06 13:37:22 -0600

    TT-4743 remove processing of export files

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ const https = require("https");
 const path = require("path");
 const mp3 = require("mp3-duration");
 const mm = require("music-metadata/lib/core");
-const { stringify } = require("querystring");
-const { format } = require("path");
 
 const host = process.env.SIL_TR_HOST;
 const stagepath = process.env.SIL_TR_URLPATH;
 
 exports.handler = async (event) => {
   const bucket = event.Records[0].s3.bucket.name;
-  const key = decodeURIComponent(
+  let key = decodeURIComponent(
     event.Records[0].s3.object.key.replace(/\+/g, " ")
   );
   console.log("hello! processing new s3 file!", key);
@@ -124,105 +122,9 @@ exports.handler = async (event) => {
       });
     return stream;
   }
-  async function FileToBuffer(filekey) {
-    var stream = null;
-    var tries = 0;
-    while (stream === null && tries < 3) {
-      try {
-        stream = await getFileStream(filekey);
-      } catch (e) {
-        console.log("key", filekey);
-        console.log(e);
-      }
-      tries++;
-    }
-    if (stream !== null) {
-      const chunks = [];
-      for await (let chunk of stream) {
-        chunks.push(chunk);
-      }
-      return Buffer.concat(chunks);
-    }
-    console.log("stream null");
-    return null;
-  }
 
-  async function putFile(filekey, body) {
-    const aws = require("aws-sdk");
-    const s3 = new aws.S3(); // Pass in opts to S3 if necessary
-    var params = {
-      Bucket: bucket,
-      Key: filekey,
-      Body: body,
-      ContentType: "application/ptf",
-    };
-    var mu = s3.upload(params);
-    var data = await mu.promise();
-    console.log("file saved ", data.Location);
-    return data.Location;
-  }
-
-  async function exportProjectMedia() {
-    const AdmZip = require("adm-zip");
-
-    console.log("export offline project file", key);
-    var start = 0;
-    var zip = new AdmZip(await FileToBuffer(key));
-    var statusFile = key.substring(0, key.length - 3) + "sss";
-    await putFile(statusFile, "0");
-    var zipEntries = zip.getEntries();
-    var deletemf = true;
-    var mediafiles = zipEntries.find(
-      (e) => e.entryName === "data/Z_attachedmediafiles.json"
-    );
-    if (!mediafiles) {
-      mediafiles = zipEntries.find(
-        (e) => e.entryName === "data/H_mediafiles.json"
-      );
-      deletemf = false;
-    }
-    if (mediafiles) {
-      var mediastr = mediafiles.getData().toString("utf8");
-      if (deletemf) zip.deleteFile(mediafiles);
-      var media = JSON.parse(mediastr);
-      if (Array.isArray(media.data)) {
-        for (var element of media.data) {
-          if (element.attributes["audio-url"]) {
-            try {
-              var buf = await FileToBuffer(element.attributes["s3file"]);
-              zip.addFile(element.attributes["audio-url"], buf);
-            } catch (e) {
-              console.log(
-                "error adding file",
-                element.attributes["audio-url"],
-                element.attributes["s3file"]
-              );
-              console.log(e);
-            }
-          }
-          start += 1;
-          if (start % 10 === 0) {
-            await putFile(statusFile, start.toString());
-          }
-        }
-        console.log("done", start);
-        //backward compatible
-        var newKey = key.substring(0, key.length - 4);
-        if (!newKey.endsWith(".ptf") && !newKey.endsWith(".audio") && !newKey.endsWith(".burrito"))
-          newKey += ".ptf";
-
-        await putFile(newKey, zip.toBuffer());
-        await putFile(statusFile, "-1");
-      }
-    }
-    return start;
-  }
   try {
-    if (key.startsWith("exports")) {
-      if (key.endsWith("tmp")) return await exportProjectMedia();
-      return 0;
-    }
-
+    if (key.startsWith("exports")) return 0; 
     if (key.startsWith("imports")) return 0; // await importProject();
 
     /* user media file */
